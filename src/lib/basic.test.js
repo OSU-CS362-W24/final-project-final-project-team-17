0b7b84dd6bd148ffeaa6bc54cb2d54883c485dd2
JackKammerer <jackkammerer16@gmail.com>
2024-03-10 00:30:48 -0800

    Added basic unit test for jest in CI/CD pipeline.

---
+++ src/lib/basic.test.js
@@ -0,0 +1,10 @@
+/*
+    This is a basic test to satisfy jest, and not break the CI/CD pipeline.
+    This should be deleted by whoever is writing unit tests, or with git pull later on.
+*/
+
+test('Basic test -- DELETE ME', () => {
+    const value = 5;
+
+    expect(value).toBe(5);
+})
