b06f3236b1fab8757cac158d5cabbd3f9393e077
San Davran <davrans@oregonstate.edu>
2024-03-14 15:40:53 -0700

    Create 1 unit test for loadAllSavedCharts, loadSavedChart, loadCurrentChartData.

--- src/lib/__tests__/chartStorage.js
+++ src/lib/__tests__/chartStorage.js
@@ -1,8 +1,56 @@
+// Mock - Thank U Perplexity. 
+
+const { JSDOM } = require('jsdom');
+const dom = new JSDOM('<!doctype html><html><body></body></html>', {
+    url: 'http://localhost'
+});
+global.window = dom.window;
+global.document = dom.window.document;
+global.localStorage = {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+}; 
 const { saveChart , loadAllSavedCharts , loadSavedChart , updateCurrentChartData , loadCurrentChartData } = require( "../chartStorage" ) ; 
 
-/** 
-test( "The romanCalculator return 'I', when given the input 1 as an integer. " , function () { 
-    expect( romanCalculator( 1 ) ).toBe( "I" ) ; 
-}) 
-**/ 
+/**
+ * @jest-environment jsdom
+ */ 
 
+require('@testing-library/jest-dom'); 
+
+// Function 2. 
+
+test( "Initial Test - loadAllSavedCharts. " , function () { 
+    window.localStorage.clear() ; 
+    const chart_1 = { type: 'line', data: [1, 2, 3] }; 
+    const chart_2 = { type: 'line', data: [1, 4, 3] }; 
+    saveChart( chart_1 ) ; 
+    saveChart( chart_2 ) ; 
+    var expectedList = [ { type: 'line', data: [1, 2, 3] } , { type: 'line', data: [1, 4, 3] } ] ; 
+    expect( loadAllSavedCharts() ).toEqual( expectedList ) ; 
+    window.localStorage.clear() ; 
+}) ; 
+
+// Function 3. 
+
+test( "Initial Test - loadSavedChart. " , function () { 
+    window.localStorage.clear() ; 
+    const chart_1 = { type: 'line', data: [1, 2, 3] }; 
+    const chart_2 = { type: 'line', data: [1, 4, 3] }; 
+    saveChart( chart_1 ) ; 
+    saveChart( chart_2 ) ; 
+    var expectedChart = { type: 'line', data: [1, 4, 3] } ; 
+    expect( loadSavedChart( 1 ) ).toEqual( expectedChart ) ; 
+    window.localStorage.clear() ; 
+}) ; 
+
+// Function 5. 
+
+test( "Initial Test - loadCurrentChartData. " , function () { 
+    window.localStorage.clear() ; 
+    const chart_1 = { type: 'line', data: [1, 2, 3] }; 
+    updateCurrentChartData( chart_1 ) ; 
+    var expectedChart = { type: 'line', data: [1, 2, 3] } ; 
+    expect( loadCurrentChartData() ).toEqual( expectedChart ) ; 
+    window.localStorage.clear() ; 
+}) ; 
