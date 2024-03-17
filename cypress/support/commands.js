ec51813dbc0adcc440668025e784e247101e534b
Jacob Cocheu <jrwcocheu@gmail.com>
2024-03-16 18:54:57 -0700

    Added testing libary to command.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,7 +2,8 @@
 // This example commands.js shows you how to
 // create various custom commands and overwrite
 // existing commands.
-//
+import '@testing-library/cypress/add-commands'
+
 // For more comprehensive examples of custom
 // commands please read more here:
 // https://on.cypress.io/custom-commands
