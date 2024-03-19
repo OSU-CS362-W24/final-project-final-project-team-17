f32ed70975c254f593f80ff43098c1364755ee64
kaden-fugate <kkadenfugate@gmail.com>
2024-03-19 14:35:40 -0700

    Removed useless test that doesnt work

--- src/line/funcs/testDisplayAlert.js
+++ src/line/funcs/testDisplayAlert.js
@@ -70,39 +70,4 @@ async function testMissingLabel(elements, X_input, Y_input){
 
 }
 
-// FUNCTION DOES NOT WORK BECAUSE ALERT IS CALLED LATER IN THE CODE WITH THE
-// PARAMETER:
-// "Error generating chart:    
-//  ReferenceError: fetch is not defined"
-async function testValidInputs(elements, X_input, Y_input, X_label_text, Y_label_text){
-
-	// do initializations: init the files, init our element object, setup our
-	// user, and start spying on window.alert. NOTE: mocking the implementation
-	// because 'window.alert' not implemented error is being throw when i dont
-	// stub the method
-	functions.initDomFromFiles(html_path, js_path);
-	functions.initElements(elements);
-	const user = userEvent.setup();
-	const alert_spy = jest.spyOn(window, 'alert');
-
-	// do actions: type in the given text for the X_input and the Y_input,
-	// click the generate chart button
-	await user.type(elements.X_label, X_label_text);
-	await user.type(elements.Y_label, Y_label_text);
-	await user.type(elements.X_inputs[0], X_input);
-	await user.type(elements.Y_inputs[0], Y_input);
-	await user.click(elements.generate_chart_button);
-		
-	// do assertions: assert that alert wasnt called with valid inputs
-	const call = alert_spy.mock.calls[0][0];
-	console.log(call);
-	expect(alert_spy).not.toHaveBeenCalled();
-
-	// this function will refresh everything for our future tests! (+ get rid
-    // of spys middleware) :)
-	await functions.resetForNextTest(elements);
-	alert_spy.mockRestore();
-
-}
-
 module.exports = ({ testMissingLabel, testMissingCoord });
